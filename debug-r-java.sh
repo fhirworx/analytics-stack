c5005b9ba4c817beb412866590d6baa0c91228cb
kriscdev <60680627+kriscdev@users.noreply.github.com>
2020-08-23 16:33:49 -0400

    Update debug-r-java.sh
    checkpoint save after compiling qt from source

--- debug-r-java.sh
+++ debug-r-java.sh
@@ -3,6 +3,7 @@ sudo mkdir /etc/profile.d && sudo chown -R ${USER} /etc/profile.d
 cat > /etc/profile.d/r.sh << EOF
 export R_HOME=/opt/R/
 export R_LD_LIBRARY_PATH=/opt/R/lib
+export LIBR_LIBRARIES=/opt/R/library
 EOF
 cat > /etc/profile.d/java.sh << EOF
 export JAVA_HOME=/opt/java
@@ -13,6 +14,9 @@ export JAR=$JAVA_HOME/bin/jar
 export _JAVA_OPTIONS="-Dio.netty.tryReflectionSetAccessible=true"
 EOF
 cat > /etc/profile.d/env.sh << EOF
+cat > /etc/profile.d/boost.sh << EOF
+export BOOST_ROOT=/opt/boost
+EOF
 export PATH="$JAVA_HOME/bin:$R_HOME/bin:$PATH"
 EOF
 #download from source, unpack, and compile
@@ -32,3 +36,24 @@ cd R_HOME
 make
 make install
 R CMD javareconf
+#install R packages
+sudo Rscript -e "install.packages(c('rJava','sparklyr', 'IRkernel', 'tm', 'openNLP', 'RWeka', 'shiny', 'officer', 'rio', 'knitr', 'rmarkdown', 'devtools', 'testthat', 'e1071', 'survival', 'ggplot2', 'mplot', 'googleVis','glmnet', 'pROC', 'data.table', 'caret', 'sqldf', 'wordcloud'), repos='https://cloud.r-project.org/')"
+#dependency for rstudio::install boost from source
+cd /opt
+wget https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.tar.gz
+tar xvf boost_1_73_0.tar.gz
+mv boost_1_73_0 boost && rm boost_1_73_0.tar.gz
+./bootstrap.sh --prefix=/opt/boost
+./b2 install
+#dependency for rstudio::install QtWebEngine from source
+git clone git://code.qt.io/qt/qt5.git
+cd /opt/qt5
+perl init-repository --module-subset=default
+perl configure
+#install R studio from source
+wget https://github.com/rstudio/rstudio/archive/v1.3.1073.tar.gz
+tar xvf v1.3.1073.tar.gz
+mv rstudio-1.3.1073 rstudio && rm v1.3.1073.tar.gz
+mkdir build
+cd build
+cmake ..  -DRSTUDIO_TARGET=Desktop -DLIBR_LIBRARIES=/opt/R/library -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/rstudio 
