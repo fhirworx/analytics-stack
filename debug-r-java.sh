60edc9e6318061cbbf57c01ff0b1ce54ba8de619
kriscdev <60680627+kriscdev@users.noreply.github.com>
2020-08-29 16:47:25 -0400

    Update debug-r-java.sh
    updates for unpacking R path into /opt/ directory

--- debug-r-java.sh
+++ debug-r-java.sh
@@ -1,3 +1,5 @@
+#requires
+swupd bundle-add R-extras
 #configs
 sudo mkdir /etc/profile.d && sudo chown -R ${USER} /etc/profile.d
 cat > /etc/profile.d/r.sh << EOF
@@ -7,19 +9,16 @@ export LIBR_LIBRARIES=/opt/R/library
 EOF
 cat > /etc/profile.d/java.sh << EOF
 export JAVA_HOME=/opt/java
-export JAVA=$JAVA_HOME/bin/java
-export JAVAC=$JAVA_HOME/bin/javac
-export JAVAH=$JAVA_HOME/bin/javah
-export JAR=$JAVA_HOME/bin/jar
+export JAVA="$JAVA_HOME/bin/java"
+export JAVAC="$JAVA_HOME/bin/javac"
+#export JAVAH="$JAVA_HOME/bin/javah"
+export JAR="$JAVA_HOME/bin/jar"
 export _JAVA_OPTIONS="-Dio.netty.tryReflectionSetAccessible=true"
 EOF
 cat > /etc/profile.d/env.sh << EOF
-cat > /etc/profile.d/boost.sh << EOF
-export BOOST_ROOT=/opt/boost
-EOF
 export PATH="$JAVA_HOME/bin:$R_HOME/bin:$PATH"
 EOF
-#download from source, unpack, and compile
+#download maven, JDK, and Rstudio from source, unpack, and compile
 cd /opt
 wget https://apache.osuosl.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 tar xvf apache-maven-3.6.3-bin.tar.gz
@@ -28,28 +27,27 @@ mv apache-maven-3.6.3 mvn && rm apache-maven-3.6.3-bin.tar.gz
 cd ~/Downloads && mv jdk-14.0.2_linux-x64_bin.tar.gz /opt
 tar xvf jdk-14.0.2_linux-x64_bin.tar.gz
 mv jdk-14.0.2 java && rm jdk-14.0.2_linux-x64_bin.tar.gz
+#install manual @ https://cran.rstudio.com/doc/manuals/r-patched/R-admin.html
 wget https://stat.ethz.ch/R/daily/R-patched.tar.gz
 tar xvf R-patched.tar.gz
 mv R-patched R && rm R-patched.tar.gz
 cd R_HOME
-./configure --prefix=opt
-make
-make install
+sudo tee -a $R_HOME/config.site<<'EOF'
+CC="gcc -m64"
+LIBnn="lib64"
+LDFLAGS="-L/usr/lib64"
+CPPFLAGS="-I/usr/include"
+CXX="g++"
+CXXFLAGS="-g -O3 -feliminate-unused-debug-types  -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=32 -Wformat -Wformat-security -Wl,--copy-dt-needed-entries -m64  -fasynchronous-unwind-tables -Wp,-D_REENTRANT -ftree-loop-distribute-patterns -Wl,-z -Wl,now -Wl,-z -Wl,relro -malign-data=abi -fno-semantic-interposition -ftree-vectorize -ftree-loop-vectorize  -fvisibility-inlines-hidden "
+LAPACK_LIBS="/usr/lib64/libopenblas.so.0"
+R_LD_LIBRARY_PATH="/usr/lib64/ruby/2.7.0/x86_64-linux-gnu/zlib.so"
+EOF
+./configure --prefix=/opt/
+make -j -O
+make install rhome=/opt/R/Rstudio 
 R CMD javareconf
 #install R packages
 sudo Rscript -e "install.packages(c('rJava','sparklyr', 'IRkernel', 'tm', 'openNLP', 'RWeka', 'shiny', 'officer', 'rio', 'knitr', 'rmarkdown', 'devtools', 'testthat', 'e1071', 'survival', 'ggplot2', 'mplot', 'googleVis','glmnet', 'pROC', 'data.table', 'caret', 'sqldf', 'wordcloud'), repos='https://cloud.r-project.org/')"
-#dependency for rstudio::install boost from source
-cd /opt
-wget https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.tar.gz
-tar xvf boost_1_73_0.tar.gz
-mv boost_1_73_0 boost && rm boost_1_73_0.tar.gz
-./bootstrap.sh --prefix=/opt/boost
-./b2 install
-#dependency for rstudio::install QtWebEngine from source
-git clone git://code.qt.io/qt/qt5.git
-cd /opt/qt5
-perl init-repository --module-subset=default
-perl configure
 #install R studio from source
 wget https://github.com/rstudio/rstudio/archive/v1.3.1073.tar.gz
 tar xvf v1.3.1073.tar.gz
