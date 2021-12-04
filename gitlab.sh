1696229ba54b78c6cf9c755b472e4a61e5db70d9
fhirworx <60680627+fhirworx@users.noreply.github.com>
2021-12-04 10:26:46 -0500

    Delete gitlab.sh

--- gitlab.sh
+++
@@ -1,9 +0,0 @@
-sudo swupd bundle-add package-utils
-cd /opt 
-mkdir gitlab
-wget --content-disposition https://packages.gitlab.com/gitlab/gitlab-ce/packages/el/7/gitlab-ce-13.7.9-ce.0.el7.x86_64.rpm/download.rpm
-mv gitlab-ce-13.6.1-ce.0.el7.x86_64.rpm ./gitlab-ce.rpm
-rpm2cpio gitlab-ce.rpm | ( cd /; cpio -idv)
-rm gitlab-ce.rpm
-sudo swupd bundle-remove package-utils
-
